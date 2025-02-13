be6edd5fe5279b9a1c7a207577c4d66b9f86588c
Shivam Lohia <contactshivamlohia@gmail.com>
2025-02-13 11:25:48 +0530

    Solution to Print a Reverse Numeric Right-Angled Triangle

--- Print a Reverse Numeric Right-Angled Triangle@@67877e1818d53eb724f435c4/code.c
+++ Print a Reverse Numeric Right-Angled Triangle@@67877e1818d53eb724f435c4/code.c
@@ -2,13 +2,13 @@
 
 int isPrime(int num) {
     if (num <= 1) {
-        return 0; 
+        return 0; // 0 and 1 are not prime
     }
     if (num <= 3) {
-        return 1; 
+        return 1; // 2 and 3 are prime
     }
     if (num % 2 == 0 || num % 3 == 0) {
-        return 0; 
+        return 0; // Check divisibility by 2 and 3
     }
     for (int i = 5; i * i <= num; i = i + 6) {
         if (num % i == 0 || num % (i + 2) == 0) {
