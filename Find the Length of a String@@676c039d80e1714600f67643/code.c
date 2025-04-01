29a5939761952fcdb81ac77e54664a5699b9f5e5
Shivam Lohia <contactshivamlohia@gmail.com>
2025-04-01 12:38:24 +0530

    Solution to Find the Length of a String

--- Find the Length of a String@@676c039d80e1714600f67643/code.c
+++ Find the Length of a String@@676c039d80e1714600f67643/code.c
@@ -4,6 +4,12 @@ int main() {
     char str[1000];
     int length = 0;
     fgets(str, sizeof(str), stdin);
+
+    if (str[0] == '\n') { 
+        printf("0\n");
+        return 0;
+    }
+
     while (str[length] != '\0' && str[length] != '\n') {
         length++;
     }
